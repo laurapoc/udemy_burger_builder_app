b3e7b7a0f2a42ba4a2b294bdee2bbddecb0c938d
laurapoc <laura.pociuniene@gmail.com>
2020-10-05 18:11:03 +0300

    add sideDrawer styling and the backdrop behind

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -17,9 +17,16 @@ const orderSummary = (props) => {
       <h3> Your Order</h3>
       <p>A deliciouns burger with a following ingredients:</p>
       <ul>{ingredientSummary}</ul>
+      <p>
+        <strong>Total Price: {props.price.toFixed(2)} </strong>
+      </p>
       <p>Continue to Checkout?</p>
-      <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-      <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
+      <Button btnType="Danger" clicked={props.purchaseCancelled}>
+        CANCEL
+      </Button>
+      <Button btnType="Success" clicked={props.purchaseContinued}>
+        CONTINUE
+      </Button>
     </Aux>
   );
 };
