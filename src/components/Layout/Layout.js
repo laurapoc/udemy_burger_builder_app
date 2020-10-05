1b069333b946866e8d6c592c85569e523a76ef73
laurapoc <laura.pociuniene@gmail.com>
2020-10-05 18:13:25 +0300

    Layout component turned into class based component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,21 @@
-import React from "react";
+import React, { Component } from "react";
 
 import Aux from "../../hoc/Auxiliary";
 import classes from "./Layout.module.css";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
-const layout = (props) => (
-  <Aux>
-    <Toolbar />
-    <SideDrawer />
-    <div>Toolbar, SideDrawer, Backdrop</div>
-    <main className={classes.Content}>{props.children}</main>
-  </Aux>
-);
+class Layout extends Component { 
+  render () {
+    return (
+      <Aux>
+      <Toolbar />
+      <SideDrawer />
+      <div>Toolbar, SideDrawer, Backdrop</div>
+      <main className={classes.Content}>{this.props.children}</main>
+    </Aux>
+    )
+  }
+}
 
-export default layout;
+export default Layout;
