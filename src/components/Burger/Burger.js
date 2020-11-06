d414e477ff3104e5c91e88268c0d92d3eb07e772
laurapoc <laura.pociuniene@gmail.com>
2020-11-06 16:37:13 +0200

    output the orders

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,6 +1,5 @@
 import React from "react";
 
-
 import classes from "./Burger.module.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
@@ -11,8 +10,6 @@ const burger = (props) => {
       return [...Array(props.ingredients[igKey])].map((_, i) => {
         return <BurgerIngredient key={igKey + i} type={igKey} />;
       });
-
-
     })
     .reduce((arr, el) => {
       return arr.concat(el);
