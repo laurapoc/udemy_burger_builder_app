9bd9ad8365365578fb4f10637fb0131f83538178
laurapoc <laura.pociuniene@gmail.com>
2020-12-14 13:53:31 +0200

    using environment variables

--- src/containers/BurgerBuilder/Checkout/ContactData/ContactData.js
+++ src/containers/BurgerBuilder/Checkout/ContactData/ContactData.js
@@ -8,7 +8,7 @@ import axios from "../../../../axios-orders";
 import Input from "../../../../components/UI/Input/Input";
 import withErrorHandler from "../../../../hoc/withErrorHandler/withErrorHandler";
 import * as actions from "../../../../store/actions/index";
-import { updateObject } from "../../../../shared/utility";
+import { updateObject, checkValidity } from "../../../../shared/utility";
 
 class ContactData extends Component {
   state = {
@@ -118,29 +118,12 @@ class ContactData extends Component {
     this.props.onOrderBurger(order, this.props.token);
   };
 
-  checkValidity(value, rules) {
-    let isValid = true;
-    // the second way to fix selecting dropdown error:
-    // if (!rules) {
-    //   return true;
-    // }
 
-    if (rules.required) {
-      isValid = value.trim() !== "" && isValid;
-    }
-    if (rules.minLength) {
-      isValid = value.length >= rules.minLength && isValid;
-    }
-    if (rules.maxLength) {
-      isValid = value.length <= rules.maxLength && isValid;
-    }
-    return isValid;
-  }
 
   inputChangedHandler = (event, inputIdentifire) => {
     const updatedFormElement = updateObject(this.state.orderForm[inputIdentifire], {
       value: event.target.value,
-      valid: this.checkValidity(event.target.value, this.state.orderForm[inputIdentifire].validation),
+      valid: checkValidity(event.target.value, this.state.orderForm[inputIdentifire].validation),
       touched: true,
     });
 
