00e4d5ee64e1dfc1457094985c0d0f5c3abae9dc
laurapoc <laura.pociuniene@gmail.com>
2021-01-02 15:36:39 +0200

    moving auth, burgerBUlder and order side effects into sagas

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import * as serviceWorker from "./serviceWorker";
 import burgerBuilderReducer from "./store/reducers/burgerBuilder";
 import orderReducer from "./store/reducers/order";
 import authReducer from "./store/reducers/auth";
-import {watchAuth} from "./store/sagas/index";
+import {watchAuth, watchBurgerBuilder, watchOrder} from "./store/sagas/index";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 // const composeEnhancers = process.env.NODE_ENV === "developement" ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
@@ -28,6 +28,8 @@ const sagaMiddleware = createSagaMiddleware();
 const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk, sagaMiddleware)));
 
 sagaMiddleware.run(watchAuth);
+sagaMiddleware.run(watchBurgerBuilder);
+sagaMiddleware.run(watchOrder);
 
 ReactDOM.render(
   <React.StrictMode>
--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,3 +1,3 @@
-export { addIngredient, removeIngredient, initIngredients } from "./burgerBuilder";
-export { purchaseBurger, purchaseInit, fetchOrders } from "./order";
+export { addIngredient, removeIngredient, initIngredients, setIngredients, fetchIngredientsFailed, } from "./burgerBuilder";
+export { purchaseBurger, purchaseInit, fetchOrders, purchaseBurgerStart, purchaserBurgerFail, purchaseBurgerSuccess, fetchOrdersStart, fetchOrdersSuccess, fetchOrdersFail } from "./order";
 export { auth, logout, setAuthRedirectPath, authCheckState, logoutSucceed, authStart, authSuccess, authFail, checkAuthTimeout } from "./auth";
